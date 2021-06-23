f9c5160c57600b941e90a32706e2374a1c28ad2f
Arnold Lee <arnoldlee60@gmail.com>
2021-06-23 12:46:04 -0500

    Getting started

---
+++ Develop/index.js
@@ -0,0 +1,16 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = 
+[   "What is the name of the project?", 
+    "What is a general description of the project"
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
